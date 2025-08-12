cc797dfb5d29fca4514f9f9d565bf32fedbfa0ca
Yeswanth Epuri <epuri.yeshu@gmail.com>
2025-08-12 08:38:22 +0530

    frontend fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -43,6 +44,8 @@ import { PublicRegisterComponent } from './components/public-register/public-reg
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [
