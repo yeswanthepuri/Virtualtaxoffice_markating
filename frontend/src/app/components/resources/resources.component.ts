ef52ec6e64a58a9543baafd8762b1d4b43cb4963
Yeswanth Epuri <epuri.yeshu@gmail.com>
2025-10-24 12:07:57 +0530

    aasync call

--- frontend/src/app/components/resources/resources.component.ts
+++ frontend/src/app/components/resources/resources.component.ts
@@ -1,36 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
   styleUrls: ['./resources.component.css']
 })
-export class ResourcesComponent implements OnInit {
-  resources: any[] = [];
-  isLoading = true;
+export class ResourcesComponent {
+  resources$: Observable<any[]>;
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.loadResources();
-  }
-
-  loadResources() {
-    console.log('Loading resources from:', `${environment.apiUrl}/resource/published`);
-    this.http.get<any[]>(`${environment.apiUrl}/resource/published`)
-      .subscribe({
-        next: (data) => {
-          console.log('Resources received:', data);
-          this.resources = data;
-          this.isLoading = false;
-        },
-        error: (error) => {
-          console.error('Error loading resources:', error);
-          this.isLoading = false;
-        }
-      });
+  constructor(private http: HttpClient) {
+    this.resources$ = this.http.get<any[]>(`${environment.apiUrl}/resource/published`);
   }
 }
 
