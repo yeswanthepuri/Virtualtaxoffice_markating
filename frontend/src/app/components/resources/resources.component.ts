e9f730974bb6ab7214412ddc338f2ea5d8424f42
Yeswanth Epuri <epuri.yeshu@gmail.com>
2025-10-24 12:24:50 +0530

    ui

--- frontend/src/app/components/resources/resources.component.ts
+++ frontend/src/app/components/resources/resources.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
   styleUrls: ['./resources.component.css']
 })
-export class ResourcesComponent {
-  resources$: Observable<any[]>;
+export class ResourcesComponent implements OnInit {
+  resources: any[] = [];
+  isLoading = true;
 
-  constructor(private http: HttpClient) {
-    this.resources$ = this.http.get<any[]>(`${environment.apiUrl}/resource/published`);
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadResources();
+  }
+
+  loadResources() {
+    this.http.get<any[]>(`${environment.apiUrl}/resource/published`)
+      .subscribe({
+        next: (data) => {
+          this.resources = data;
+          this.isLoading = false;
+        },
+        error: (error) => {
+          console.error('Error loading resources:', error);
+          this.isLoading = false;
+        }
+      });
   }
 }
 
